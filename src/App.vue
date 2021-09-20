3f60bc4b90348b40b6f4ee40f55632ed50e439ce
ianbelfatto <ibelfatto@gmail.com>
2021-09-20 16:07:44 -0400

    adds css gradient

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,13 @@
 
 html {
   height: 100%;
-  /* background: rgb(34, 193, 195); */
-  /* background: linear-gradient(120deg, rgba(34, 193, 195, 1) 0%, rgba(253, 187, 45, 1) 100%); */
+  background: #00f260; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to top, #0575e6, #00f260); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to top,
+    #0575e6,
+    #00f260
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
 body {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="logo">
-      <img src="../assets/relaxrlogot.png" alt="" />
+      <img src="../assets/relaxrlogo.png" alt="" />
     </div>
     <br />
     <br />
